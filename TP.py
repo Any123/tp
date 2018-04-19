f5f35ab0c4d6bf05cf90e9f048eb1694669675e2
anya <anya@gamil.com>
2018-04-19 14:50:10 +0200

    rendu final

---
+++ TP.py
@@ -0,0 +1,108 @@
+#Good
+def exo01():
+    i = 0
+    while i < 500:
+        print("Je dois faire des sauvegardes régulières de mes fichiers.")
+        i = i + 1
+
+exo01()
+
+def exo2():
+
+    while i < 1500:
+        i
+        i = i + 2
+    print(i)
+
+for i in range(1, 20):
+        rlt = i + 2
+
+        print(rlt)
+
+exo2()
+
+def exo3():
+
+    nbr = 1
+    while nbr <= 10:
+        print(nbr, '* 13 =', nbr * 13)
+        nbr += 1
+    print("Et voilà !")
+
+exo3()
+
+
+def exo4():
+
+    a = input("Entrez un mot, svp :")
+    print(len(a))
+
+exo4
+
+
+def exo5():
+
+    a = int(input("entrez un nombre :"))
+
+    if a%2==0:
+        print("pair")
+    else:
+        print("impair")
+exo5()
+
+def exo6():
+
+    a = int(input("entrez un chiffre, svp :"))
+    if a < 10:
+        print("plus grand!")
+    elif > 20:
+        print("plus petit!")
+    else:
+        print("Good !")
+
+exo6()
+
+def exo7():
+    a = int(input("entrez un chiffre, svp :"))
+    for S in range(a+1, a+11):
+        print(S)
+
+exo7()
+
+def exo8():
+    nbr = 1
+    a = int(input("entrez un chiffre, svp :"))
+    while nbr <= 10:
+        print(nbr * a)
+        nbr += 1
+    print("Et voilà !")
+exo8()
+
+
+def exo9():
+    a = int(input("entrez un chiffre, svp :"))
+    base = 1
+    max = 10
+        for 
+exo9
+
+def exo10():
+
+    a = int(input("entrez un chiffre, svp :"))
+
+    if a in range(6, 8):
+        print("Poussin")
+    if a in range(8, 10):
+        print("Pupille")
+    if a in range(10, 12):
+        print("Minime")
+    if a >= 12:
+        print("Cadet")
+
+exo10()
+
+
+def exo11():
+
+
+exo11
